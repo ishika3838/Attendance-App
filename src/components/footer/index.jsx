3f7b543e5851d7dbd975c0265d1fee4f64bee49c
kaifi ahmed <kaifiahmed12k@gmail.com>
2023-08-20 22:23:13 +0530

    fixed footer folder

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Wrapper } from './style'
+
+const Footer = () => {
+  return (
+   <Wrapper className=''>
+      copyright@2023 AttendEase || All rights reserved.
+      </Wrapper>
+ 
+
+ )
+}
+
+export default Footer
---
+++ src/components/footer/style.js
@@ -0,0 +1,17 @@
+import { styled } from "styled-components"
+
+export const Wrapper = styled.footer`
+color:black;
+background-color:white;
+text-align: center;
+margin-bottom:2px;
+position: fixed;
+ bottom: 0;
+//  left: 1%;
+//  right: 1%;
+
+ background:black;
+ width:100%;
+ color white :
+
+  `;
