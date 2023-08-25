bd2040f595ab033cccdc4617f7368e276fcf0c02
kaifi ahmed <kaifiahmed12k@gmail.com>
2023-08-25 22:17:35 +0530

    fix error

--- src/components/register/index.jsx
+++ src/components/register/index.jsx
@@ -112,7 +112,7 @@ const Register = ({ handleRegister, role, setSelectedRole }) => {
             />
           </div>
 
-          <div>
+          
           <button type="submit">Register</button>
         </form>
         <p>
